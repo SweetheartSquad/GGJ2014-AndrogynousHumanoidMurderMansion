2c84307e7d887d5623e791b62b4e66fa87565869
ianmart1n <ian.martin@rogers.com>
2014-01-26 11:17:32 -0500

    basic main menu navigation
    you can press keys and see words explode in blood

--- source/InstructState.hx
+++ source/InstructState.hx
@@ -11,20 +11,17 @@ import flixel.util.FlxMath;
 /**
  * A FlxState which can be used for the game's menu.
  */
-class MenuState extends FlxState
+class InstructState extends FlxState
 {
 	private var _text1:FlxText;
 	private var _text2:FlxText;
 	private var particles:FlxGroup;
 	private var counter:Float = 0;
-	private var pReady:Bool;
-	private var qReady:Bool;
+	private var ready:Bool;
 	
 	override public function create():Void
 	{
-		pReady = false;
-		qReady = false;
-		
+		ready = false;
 		// Set a background color
 		FlxG.cameras.bgColor = 0x00000000;
 		// Show the mouse (in case it hasn't been disabled)
@@ -32,15 +29,10 @@ class MenuState extends FlxState
 		FlxG.mouse.show();
 		#end
 		
-		_text1 = new FlxText((Reg.gameWidth / 2) - 100, Reg.gameHeight / 4 + 250, 295, "PRESS SPACE TO START");
+		_text1 = new FlxText((Reg.gameWidth / 2) - 100, Reg.gameHeight / 4 + 250, 290, "PRESS Q TO GO BACK");
 		_text1.size = 20;
 		_text1.color = 0x000000;
 		add(_text1);
-		
-		_text2 = new FlxText((Reg.gameWidth / 2) - 95, Reg.gameHeight / 4 + 285, 300, "PRESS Q TO READ INSTRUCTIONS");
-		_text2.size = 14;
-		_text2.color = 0x000000;
-		add(_text2);
 		
 		particles = new FlxGroup();
 		add(particles);
@@ -51,7 +43,6 @@ class MenuState extends FlxState
 	override public function destroy():Void
 	{
 		_text1 = null;
-		_text2 = null;
 		super.destroy();
 	}
 
@@ -78,37 +69,19 @@ class MenuState extends FlxState
 	{
 		super.update();
 		
-		if (FlxG.keyboard.anyJustReleased(["SPACE"])) {
+		if (FlxG.keyboard.anyJustReleased(["Q"])) {
 			_text1.alpha = 0;
-			pReady = true;
+			ready = true;
 			
 			for (i in Math.round(_text1.x)...Math.round(_text1.x + _text1.width)) {
 				makeGibs(i, _text1.y);
 			}
-			
 		}
-		
-		if (FlxG.keyboard.anyJustReleased(["Q"])) {
-			_text2.alpha = 0;
-			qReady = true;
 			
-			for (i in Math.round(_text2.x)...Math.round(_text2.x + _text2.width)) {
-				makeGibs(i, _text2.y);
-			}
-			
-		}
-		
-		if(pReady == true){
+		if(ready == true){
 			counter += FlxG.elapsed;
 			if (counter >= 1) {
-				FlxG.switchState(new PlayState());
-			}
-		}
-		
-		if(qReady == true){
-			counter += FlxG.elapsed;
-			if (counter >= 1) {
-				FlxG.switchState(new InstructState());
+				FlxG.switchState(new MenuState());
 			}
 		}
 		
